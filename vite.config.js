88a11a0d56b9878cc9ec4a2ecdc2ca64a6e2c20b
juanibosco <20478856+juanibosco@users.noreply.github.com>
2025-04-08 10:16:28 -0300

    feat: update Vite to version 6.2.5 and add PWA support with manifest configuration
    - Updated Vite dependency in package.json
    - Added vite-plugin-pwa for Progressive Web App support
    - Created a new PWA manifest with app details and icons
    - Added cartas.png image for PWA icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Anotador de Truco',
+        short_name: 'Truco',
+        description: 'Anotador de truco desarrollado en React JS.',
+        theme_color: '#242424',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/cartas.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/cartas.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
